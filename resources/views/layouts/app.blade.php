df85b9a85ae9d2421b2e71132414a0c80d1906fc
ychakola <yc566@njit.edu>
2018-04-27 12:43:22 -0400

    Profile editing and messages have been setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,11 @@
     </nav>
 
     <main class="py-4">
+        <div class="col-12">
+        @include('flash.error')
+        @include('flash.messages')
+        @include('flash.status')
+        </div>
         @yield('content')
     </main>
 </div>
