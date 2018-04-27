7bd062687586f14e55d434ec713ebb2900369660
ychakola <yc566@njit.edu>
2018-04-27 13:19:00 -0400

    Creating and viewing question have been setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,9 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Questions
+                        <a class="btn btn-primary float-right" href="{{ route('question.create') }}">
+                            Create a Question
+                        </a>
 
 
                         <div class="card-body">
@@ -35,9 +38,9 @@
                                         </div>
                                     </div>
                                 @empty
-                                    <a class="btn btn-primary" href="{{ route('question.create') }}">
-                                        There are no questions to view,click to create a question.
-                                    </a>
+
+                                        There are no questions to view,you can create a question.
+
                                 @endforelse
                             </div>
 
